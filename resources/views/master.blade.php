c475f89693b7b6e9c424afc604b8e142492df6ca
hafidabi <abi.daniswara@gmail.com>
2020-07-12 20:41:21 +0700

    heroku comit

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,13 +8,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{url('/adminlteres/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="/adminlteres/plugins/fontawesome-free/css/all.min.css">
   <!-- editor -->
   <link rel="stylesheet" href="{{url('/adminlteres/plugins/summernote/summernote-bs4.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{url('/adminlteres/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="/adminlteres/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   {{-- jquery --}}
